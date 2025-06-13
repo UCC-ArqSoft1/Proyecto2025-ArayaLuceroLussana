762e92028707451ba5d9c1431ccf3e6dccef54ac
MarianoAraya <arayamariano67@gmail.com>
2025-06-13 18:29:03 -0300

    Ultimo push

--- backend/config/config.go
+++ backend/config/config.go
@@ -19,28 +19,28 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	err := godotenv.Load()
+	err := godotenv.Load() //Intenta cargar las variables de entorno desde el archivo .env
 	if err != nil {
 		log.Println("The file .env could not be charged, using system variables")
 	}
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", //Crea la cadena de conexion DSN
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_NAME"),
 	)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{}) //Abre la conexion a la base de datos usando GORM y el driver MySQL
 	if err != nil {
 		log.Fatalf("Error connecting to the database: %v", err)
 	}
 
 	DB = db
-	fmt.Println("Database connected successfully")
+	fmt.Println("Database connected successfully") //Guarda la conexion en una variable local
 
 	//Migracion de los modelos a la base de datos
-	err = db.AutoMigrate(&models.User{}, &models.Activity{}, &models.Inscription{})
+	err = db.AutoMigrate(&models.User{}, &models.Activity{}, &models.Inscription{}) //Crea o actualiza las tablas en la base de datos
 	if err != nil {
 		log.Fatalf("Failed to auto-migrate models: %v", err)
 	}
